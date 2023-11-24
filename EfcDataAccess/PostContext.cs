7278c1dda72eb1238aa7f4a0d2940389b04fd95b
xcati <307040@viauc.dk>
2023-11-24 20:09:16 +0100

    fds

--- EfcDataAccess/PostContext.cs
+++ EfcDataAccess/PostContext.cs
@@ -12,7 +12,7 @@ public class PostContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite("Data Source = C:\\Users\\catin\\RiderProjects\\DNP Assignment\\EfcDataAccess\\Todo.db");
+        optionsBuilder.UseSqlite("Data Source = C:/Users/catin/RiderProjects/AssignmentDNP/EfcDataAccess/Post.db");
         // C:\Users\catin\RiderProjects\DNP Assignment\EfcDataAccess\Todo.db
         optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);            
         optionsBuilder.LogTo(Console.WriteLine); // Add this line for logging
